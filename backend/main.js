ae4c10441376518cc0c305e74fb73675a4463415
Gilmanalfathir <gilmanalfathir23@gmail.com>
2024-11-08 05:55:35 +0700

    backend commit

--- backend/main.js
+++ backend/main.js
@@ -1,15 +1,24 @@
+require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const connectDB = require('./config/db');
+const authRoutes = require('./routes/auth');
+
 const app = express();
 
 // Connect Database
 connectDB();
 
 // Init Middleware
-app.use(express.json({ extended: false }));
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+  }));
+app.use(express.json());
 
 // Define Routes
-app.use('/api/auth', require('./routes/auth'));
+app.use('/api/auth', authRoutes);
+
 // Add more routes like /api/products, /api/negotiations, etc.
 
 // Set up the server to listen
